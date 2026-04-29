af61f7ff766b2d4781e0982c850db88b896cd163
mdmarufhossain1103-ship-it <mdmarufhossain1103@gmail.com>
2026-04-29 23:04:34 +0600

    Added navbar in my website

--- src/components/shered/NavbarPage.jsx
+++ src/components/shered/NavbarPage.jsx
@@ -1,28 +1,42 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@heroui/react";
+
+import Link from 'next/link';
 import logo from '../../assets/logo.jpg'
 import Image from 'next/image';
 
 const NavbarPage = () => {
+    const links = <>
+    <Link href={'/'}>Home</Link>
+    <Link href={'/product'}>Product</Link>
+    <Link href={'/profile'}>My Profile</Link>
+    </>
     return (
         <div>
-            <Navbar>
-                <NavbarBrand>
-                    {/* <Logo />
-                    <p className="font-bold">ACME</p> */}
-                    <Image src={logo} alt='logo' width={100} height={100}></Image>
-                </NavbarBrand>
-                <NavbarContent>
-                    <NavbarItem><Link href="#">Features</Link></NavbarItem>
-                    <NavbarItem><Link href="#">Pricing</Link></NavbarItem>
-                </NavbarContent>
-            </Navbar>
-            {/* With right-aligned content */}
-            <Navbar>
-                <NavbarBrand>Logo</NavbarBrand>
-                <NavbarContent justify="end">
-                    <NavbarItem><Button>Sign Up</Button></NavbarItem>
-                </NavbarContent>
-            </Navbar>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {links}
+                        </ul>
+                    </div>
+                    <Image src={logo} alt='logo' width={50} height={50}></Image>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-5 text-xl font-semibold">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <div className='flex gap-2 items-center'>
+                        <Image src={logo} alt='logo' width={50} height={50}></Image>
+                        <Link className='btn btn-primary' href={'/login'}>Login</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
