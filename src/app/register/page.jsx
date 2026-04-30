16a1664cceb28300424dc3c724fae9e2b7ae0ba6
mdmarufhossain1103-ship-it <mdmarufhossain1103@gmail.com>
2026-04-30 22:30:09 +0600

    Added registar page

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -4,6 +4,40 @@ import { error } from 'better-auth/api';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+const RegisterPage = () => {
+    const { register, handleSubmit, formState: { errors }, } = useForm();
 
+    const handleRegister = async (data) => {
+        const { name, email, password } = data;
+
+        const { data: res, error } = await authClient.signUp.email({
+            name: name,
+            email: email,
+            password: password,
+            callbackURL: '/',
+        })
+
+        if (error) {
+            alert(error.message)
+        }
+        if (res) {
+            alert("Registered successfully: ", res)
+        }
+    }
+    return (
+        <div className='w-full flex min-h-[80vh] items-center justify-center my-20'>
+            <form onSubmit={handleSubmit(handleRegister)} className='w-full max-w-md flex flex-col p-5 gap-5 shadow-lg rounded-lg border'>
+                <h1 className='text-2xl font-bold text-center my-5'>Register</h1>
+                <input type="name" {...register("name", { required: "Name field is required" })} className='border p-3 rounded-xl' placeholder='Enter your name' />
+                {errors.name && <p className='text-red-500'>*{errors.name.message}</p>}
+                <input type="email" {...register("email", { required: "Email field is required" })} className='border p-3 rounded-xl' placeholder='Enter your email' />
+                {errors.email && <p className='text-red-500'>*{errors.email.message}</p>}
+                <input type="password" {...register("password", { required: "Password field is required" })} className='border p-3 rounded-xl' placeholder='Enter your password' />
+                {errors.password && <p className='text-red-500'>*{errors.password.message}</p>}
+                <button className='btn btn-primary mb-3' type='submit'>Submit</button>
+            </form>
+        </div>
+    );
+};
 
 export default RegisterPage;
